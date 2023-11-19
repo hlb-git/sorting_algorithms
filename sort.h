f9846ab3cd86b04b31178e8c7bd9bbcc54e6d234
Ngozi Aguluka <melissacinta@gmail.com>
2023-11-19 18:25:03 +0000

    added function for quick sort

--- sort.h
+++ sort.h
@@ -13,13 +13,17 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void swap(int *a, int *b);
 
 #endif
