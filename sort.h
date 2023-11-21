261182c3cc68406159a2efdc88618ca4b64d6df0
Ngozi Aguluka <melissacinta@gmail.com>
2023-11-21 05:07:08 +0000

    removed declarations

--- sort.h
+++ sort.h
@@ -22,8 +22,5 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
-void quick_sort_recursive(int *array, int low, int high, size_t size);
-int lomuto_partition(int *array, int low, int high, size_t size);
-void swap(int *a, int *b);
 
 #endif
