cba89bcb5b9cd20f305f36ae13f06edbc036ba92
Love <mzeelovegeneral@gmail.com>
2023-11-21 22:34:47 +0100

    fixed failing checks

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,23 +9,24 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, temp, init_pos;
 
-	if (array && size)
+	if (!array || size < 2)
+	
+	for (i = 0; i < size - 1; i++)
 	{
-		for (i = 0; i < size - 1; i++)
+		init_pos = i;
+		for (j = i + 1; j < size; j++)
 		{
-			init_pos = i;
-			for (j = i; j < size; j++)
-			{
-				if (array[j] < array[init_pos])
-					init_pos = j;
-			}
-			if (init_pos != i)
-			{
-				temp = array[i];
-				array[i] = array[init_pos];
-				array[init_pos] = temp;
-			}
+			if (array[j] < array[init_pos])
+				init_pos = j;
+		}
+		if (init_pos != i)
+		{
+			temp = array[i];
+			array[i] = array[init_pos];
+			array[init_pos] = temp;
 			print_array(array, size);
 		}
+		
 	}
+	
 }
