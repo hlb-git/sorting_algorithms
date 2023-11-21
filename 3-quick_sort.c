8ce036ba28ba5ebf304f643814ebecac3abe040a
Ngozi Aguluka <melissacinta@gmail.com>
2023-11-21 05:41:00 +0000

    fixed function

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,19 +6,6 @@ int lomuto_partition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
 
 /**
- * quick_sort - Sorts an array of integers in ascending order using Quick sort
- * @array: The array to be sorted
- * @size: The size of the array
- */
-void quick_sort(int *array, size_t size)
-{
-	if (array == NULL || size < 2)
-		return;
-
-	quick_sort_recursive(array, 0, size - 1, size);
-}
-
-/**
  * quick_sort_recursive - Recursive function for Quick sort
  * @array: The array to be sorted
  * @low: The low index of the partition
@@ -29,7 +16,7 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
 	int pivot;
 
-	if (low < high)
+	if (high - low > 0)
 	{
 		pivot = lomuto_partition(array, low, high, size);
 		quick_sort_recursive(array, low, pivot - 1, size);
@@ -48,25 +35,28 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-	int pivot = array[high];
-	int i = low - 1, j;
+	int *pivot, i, j;
 
-	for (j = low; j < high; j++)
+	pivot = array + high;
+	for (j = i = low; j < high; j++)
 	{
-		if (array[j] <= pivot)
+		if (array[j] < *pivot)
 		{
-			i++;
-			if (i != j)
+			if (i < j)
 			{
-				swap(&array[i], &array[j]);
+				swap(array + j, array + i);
 				print_array(array, size); /* Print the array after each swap */
 			}
+			i++;
 		}
 	}
-
-	swap(&array[i + 1], &array[high]);
-	print_array(array, size); /* Print the array after each swap */
-	return (i + 1);
+	
+	if (array[i] > *pivot)
+	{
+		swap(array + i, pivot);
+		print_array(array, size); /* Print the array after each swap */
+	}
+	return (i);
 }
 
 /**
@@ -80,4 +70,17 @@ void swap(int *a, int *b)
 
 	*a = *b;
 	*b = temp;
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using Quick sort
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_recursive(array, 0, size - 1, size);
 }
